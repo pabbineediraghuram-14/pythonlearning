c50965e2cf751fb5c9440f3624842fe0076bf8a9
pabbineediraghuram-14 <pabbineediraghuram@gmail.com>
2026-05-05 15:33:47 +0530

    updated

---
+++ day1/prob1.py
@@ -0,0 +1,17 @@
+a = 5.90
+b = 7.90
+ch = "-"
+if ch == "+":
+    c = a + b
+    print(c)
+elif ch == "-":
+    c = a-b
+    print(c)
+elif ch == "*":
+    c = a*b
+    print(c)
+elif ch == "/":
+    c = a/b
+    print(c)
+else:
+    print("invalid operation")
