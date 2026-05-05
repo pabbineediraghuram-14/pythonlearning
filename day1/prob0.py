1e9a08ff3439453b82a1e73a3fea9fc41cbc507d
pabbineediraghuram-14 <pabbineediraghuram@gmail.com>
2026-05-05 14:26:13 +0530

    updated

---
+++ day1/prob0.py
@@ -0,0 +1,10 @@
+import time
+ 
+ 
+for n in range(0,101):
+    if n%2 == 0:
+        print("fizz")
+    else:
+        print("fuzz")
+        
+    time.sleep(1)
--- day1/prob4.py
+++ day1/prob4.py
@@ -1,1 +1,10 @@
-ECHO is on.
+#print phythogoroeas number
+for x in range(1,100):
+    for y in range(x,100):
+        for z in range(y,100):
+            if z*z == x*x + y*y:
+                print(x,y,z)
+         
+         
+ 
+    
