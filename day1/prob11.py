f868a4be7cad31614e2d021b51c0dbea97d23945
pabbineediraghuram-14 <pabbineediraghuram@gmail.com>
2026-05-06 14:22:54 +0530

    updated

---
+++ day1/prob11.py
@@ -0,0 +1,26 @@
+#11(1) question
+import random
+
+score = 0
+for i in range(10):
+    user = 90
+    code = random.randint(1,101)
+    if user < code:
+        score_user = -1
+        score += score_user
+    elif user == code:
+        score_user = 3
+        score += score_user
+    elif user > code:
+        score_user = 1
+        score += score_user
+        
+print(score)    
+if score > 0:
+    print("User wins")
+elif score < 0:
+    print("Code wins")
+
+   
+    
+
