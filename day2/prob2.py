ce45fd3396791df3b4d8cc80ae7e7224e53e27d5
pabbineediraghuram-14 <pabbineediraghuram@gmail.com>
2026-05-09 16:51:14 +0530

    updated

---
+++ day2/prob2.py
@@ -0,0 +1,13 @@
+a = ["OK", "NOK", "hello", "hi"]
+print(a[3])
+print(a[2],a[0])
+a[3] = "hello"
+print(a)
+print("Length of 2nd element:", len(a[1]))
+print(a[0],a[1],a[-2],a[-1])
+for i in a:
+    print("length of ",i,"word",len(i))
+   
+   
+for i in a:
+    print(i)
