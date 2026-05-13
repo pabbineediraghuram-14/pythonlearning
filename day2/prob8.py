e612831e9d80c0a836ebc0697a30b8dcce85468b
pabbineediraghuram-14 <pabbineediraghuram@gmail.com>
2026-05-13 14:50:22 +0530

    updated

--- day2/prob8.py
+++ day2/prob8.py
@@ -1,9 +1,10 @@
 #print phythogoroeas number
+import math
 lst = []
 for x in range(1,100):
     for y in range(x,100):
         for z in range(y,100):
-            if z*z == x*x + y*y:
+            if z*z == sum([x*x , y*y]):
                 lst.append((x,y,z))
          
-print(lst)
+print(f"the required list{lst}")
