f389c4aa0d39edbeddea08de14764ecc58735d47
pabbineediraghuram-14 <pabbineediraghuram@gmail.com>
2026-05-17 21:52:00 +0530

    updated

--- day3/prob6.py
+++ day3/prob6.py
@@ -1,22 +1,15 @@
 D1 = {'ok': 1, 'nok': 2}
 D2 = {'ok': 2, 'new':3 }
-t = D1 | D2
-print(t)
+print(D1 | D2)
 m = {} 
 for i in D1:
     for j in D2:
         if i == j:
             v = D1[i] + D2[j]
             m[i] = v
-       
-
-for k in D1:
-    if k not in m:
-        m[k] = D1[k]
-        
-    
-for l in D2:
-    if l not in m:
-        m[l] = D2[l]
+        if i not in m:
+            m[i] = D1[i]
+        if j not in m:
+            m[j] = D2[j]
         
 print(m)
