b6ef8e415d5499320d406f39e4feace1183761d7
pabbineediraghuram-14 <pabbineediraghuram@gmail.com>
2026-05-28 13:36:59 +0530

    updated

---
+++ day3/prob6(2).py
@@ -0,0 +1,26 @@
+
+D1 = {'ok': 1, 'nok': 2}
+D2 = {'ok': 2, 'new':3 }
+
+DU = {}
+for i in D1:
+    DU[i] = D1[i]
+
+    
+for j in D2:
+    DU[j] = D2[j]
+    
+print(DU)
+DI = {}
+for i in D1:
+    if i in D2:
+        DI[i] = D1[i]
+        
+        
+print(DI)
+DD = {}
+for i in D1:
+    if i not in D2:
+        DD[i] = D1[i]
+        
+print(DD)        
